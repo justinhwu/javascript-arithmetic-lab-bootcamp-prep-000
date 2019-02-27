0f903a60aa8d13fd3410dbb60f9fa9e199b050aa
Justin Wu <justinhwu95@gmail.com>
2019-02-27 18:35:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-var number = 10
-function add5(){
-  number +=5
+function add(a,b){
+  return a+b
 }
-
-function divideBy3(){
-  number /=3
-}
-
-divideBy3()
-console.log(number)
-add5()
-console.log(number)
-
-number =10
-add5()
-console.log(number)
-
-divideBy3()
-console.log(number)
